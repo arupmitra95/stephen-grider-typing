4b4f7dd2311bbc881f99b2ffad745e841e4cd10d
Arup Mitra <arupmitra95@gmail.com>
2022-02-23 20:38:33 +0530

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import faker from '@faker-js/faker';
 
-faker.lorem.sentence();
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Typing Challange';
+  randomText = faker.lorem.sentence();
 }
