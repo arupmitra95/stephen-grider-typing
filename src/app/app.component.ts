e256fd4d000a65066fea9487cd51b11c0ea6a69b
Agomani Ray <arupmitra95@gmail.com>
2022-02-23 13:16:28 +0530

    favicon added and also changed the title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'stephen-grider-typing';
+  title = 'Typing Challange';
 }
